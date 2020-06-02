5a81b92eb818b590d41766e520117f9ed58cce4f
liuzhipeng <492782442@qq.com>
2020-06-02 18:35:29 +0800

    upload

---
+++ simpleimage-demo/src/main/java/com/lzp/ImageCompress.java
@@ -0,0 +1,53 @@
+package com.lzp;
+
+import com.alibaba.simpleimage.ImageRender;
+import com.alibaba.simpleimage.SimpleImageException;
+import com.alibaba.simpleimage.render.*;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ImageCompress {
+
+    public static void main(String[] args) {
+        //原图片
+        File in = new File("/Users/mac/Downloads/压缩前.jpeg");
+
+        //压缩后
+        File out = new File("/Users/mac/Downloads/压缩后.jpeg");
+
+        //前面两个参数是宽高，最后一个参数是压缩算法
+        ScaleParameter scaleParam = new ScaleParameter(178, 178, ScaleParameter.Algorithm.AUTO);  //将图像缩略到1024x1024以内，不足1024x1024则不做任何处理
+
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+        ImageRender wr = null;
+        try {
+            inStream = new FileInputStream(in);
+            outStream = new FileOutputStream(out);
+            ImageRender rr = new ReadRender(inStream);
+            ImageRender sr = new ScaleRender(rr, scaleParam);
+            wr = new WriteRender(sr, outStream);
+
+            wr.render();                            //触发图像处理
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(inStream);         //图片文件输入输出流必须记得关闭
+            IOUtils.closeQuietly(outStream);
+            if (wr != null) {
+                try {
+                    wr.dispose();                   //释放simpleImage的内部资源
+                } catch (SimpleImageException ignore) {
+                    // skip ...
+                }
+            }
+        }
+
+
+
+    }
+
+}
