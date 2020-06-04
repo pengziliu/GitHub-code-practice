ba9e7078074f2ad4a3d7d36eacdcc986c099e2cf
liuzhipeng <492782442@qq.com>
2020-06-04 11:32:32 +0800

    xxjob

---
+++ xxx-job-demo/xxl-job-admin/src/main/java/com/xxl/job/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.xxl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenan
+ * date 2020-06-01
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
